f58ed5d9307c7862f59dbac9931c5577a683743a
Demkiv Maxym <demkivmaxym@gmail.com>
2026-05-23 16:02:49 +0300

    feat: implement product rendering from array using forEach and if/else

---
+++ main.js
@@ -0,0 +1,56 @@
+let products = [
+    {id: 1, name: "Помідори", amount: 2, isBought: true, isEditing: false},
+    {id: 2, name: "Печиво", amount: 2, isBought: false, isEditing: false},
+    {id: 3, name: "Сир", amount: 1, isBought: false, isEditing: false}
+]
+
+const productsContainer = document.getElementById("products-container");
+const leftStatsContainer = document.getElementById("left-stats-container");
+const boughtStatsContainer = document.getElementById("bought-stats-container");
+
+const searchInput = document.querySelector(".search-input");
+const addProductBtn = document.querySelector(".add-product-btn");
+
+function render() {
+    productsContainer.innerHTML = "";
+    leftStatsContainer.innerHTML = "";
+    boughtStatsContainer.innerHTML = "";
+
+    products.forEach(product => {
+        let itemHtml = '';
+
+         if (product.isBought) {
+            itemHtml = `
+                <div class="item-container" data-id="${product.id}">
+                    <span class="item-name" id="bought-name">${product.name}</span>
+                    <div class="quantity-block">
+                        <span class="count-number">${product.amount}</span>
+                    </div>
+                    <div class="item-actions">
+                        <button class="buy-btn" data-tooltip="Позначити як не куплено">Не куплено</button>
+                    </div>
+                </div>
+            `;
+        } else {
+            itemHtml = `
+                <div class="item-container" data-id="${product.id}">
+                    <span class="item-name">${product.name}</span>
+                    <div class="quantity-block">
+                        <button class="count-btn minus" data-tooltip="Зменшити кількість">−</button>
+                        <span class="count-number">${product.amount}</span>
+                        <button class="count-btn plus" data-tooltip="Збільшити кількість">+</button>
+                    </div>
+                    <div class="item-actions">
+                        <button class="buy-btn" data-tooltip="Позначити як куплено">Куплено</button>
+                        <button class="delete-btn" data-tooltip="Видалити товар">×</button>
+                    </div>
+                </div>
+            `;
+        }
+
+        productsContainer.insertAdjacentHTML("beforeend", itemHtml);
+    });
+
+}
+
+render();
